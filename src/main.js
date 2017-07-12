2a66f06d69d8fdd259f4f375de091d9896207570
Eugene Kagan <eugene@Eugenes-MacBook-Pro-2.local>
2017-07-12 06:24:53 -0400

    simple routing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,36 @@
 $ = jQuery = require('jquery');
 
-var App = console.log('Hello from Browserify');
+const React = require('react');
+const Home = require('./components/homePage');
+const About = require('./components/about/aboutPage');
 
-module.exports = App;
+const App = React.createClass({
+  render: function () {
+    let Child;
+
+    switch (this.props.route) {
+      case 'about':
+        Child = About;
+        break;
+      default:
+        Child = Home;
+    }
+
+    return (
+      <div>
+        <Child/>
+      </div>
+    );
+  }
+});
+
+
+function render() {
+  const htmlRoot = document.getElementById('app');
+  const route = window.location.hash.substr(1);
+  React.render(<App route={route}/>, htmlRoot);
+}
+
+// event that occurs when there's hash change in the url
+window.addEventListener('hashchange', render);
+render();
